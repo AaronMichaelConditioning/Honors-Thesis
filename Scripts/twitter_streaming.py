26e0e90003696c02e037f6d39a144c5528df8f3c
parthmishra <parthmishra@Parths-MacBook-Pro.local>
2016-09-19 23:35:32 -0600

    added timing functionality

--- Scripts/twitter_streaming.py
+++ Scripts/twitter_streaming.py
@@ -2,6 +2,8 @@
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
+import time
+import sys
 
 #Variables that contains the user credentials to access Twitter API 
 access_token = "772083660484780032-KJ0DHZ535tA9KSXKp7gOLkLh5jUm5zF"
@@ -10,11 +12,21 @@ consumer_key = "JlapdjTbD5x988S6K8O8oHUlk"
 consumer_secret = "rxYBSHX44Drskh1vhwTzoRTegs33yEgbgusURfOakRjUenH6JE"
 
 
+access_tokenPM = "166860232-E7XDQFvUrqan4TT5T6JFfpPoUctPnXesXdmTPnth"
+access_token_secretPM = "60IizseD6AYQu7Azyu8OOsAJsMuO1KfHo2sUwP3LSBxsI"
+consumer_keyPM = "zKoQbJOhJr91wFCV5UTiQLI64"
+consumer_secretPM = "BEOx061eFPn1QhWqjN3vGKcBR5sR51Go2auiaPz5dAnPwsK3Bo"
+
 #This is a basic listener that just prints received tweets to stdout.
 class StdOutListener(StreamListener):
 
     def on_data(self, data):
-        print data
+        timeLimit = 15
+        if (time.time() - startTime) < timeLimit:
+            print data
+        else:
+            #print "End of Stream"
+            sys.exit()
         return True
 
     def on_error(self, status):
@@ -24,10 +36,14 @@ class StdOutListener(StreamListener):
 if __name__ == '__main__':
 
     #This handles Twitter authetification and the connection to Twitter Streaming API
+    timeLimit = 15
+    startTime = time.time()
     l = StdOutListener()
-    auth = OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
+    auth = OAuthHandler(consumer_keyPM, consumer_secretPM)
+    auth.set_access_token(access_tokenPM, access_token_secretPM)
     stream = Stream(auth, l)
 
-    #This line filter Twitter Streams to capture data by the keywords: 'python', 'javascript', 'ruby'
+        #This line filter Twitter Streams to capture data by the keywords: 'python', 'javascript', 'ruby'
     stream.filter(track=['Trump', 'Clinton'])
+
+   
